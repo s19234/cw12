41c75a0888bdecb09c5db4a76e62a11f8f72c015
Ryszard <s19234@pjwskt.edu.pl>
2020-05-25 12:24:10 +0200

    Initial and last commit I suppose

---
+++ Ćwiczenia12/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Ćwiczenia12.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ćwiczenia12.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options: options)
+        { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Ćwiczenia12/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ćwiczenia12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
